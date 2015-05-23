194f34de857b5ae782b0a28cb89aa245ad7f3aee
Aleksandar Bojchevski <aleksandar.bojchevski@gmail.com>
2015-05-23 15:16:46 +0200

    Added simple word value feature generator

---
+++ source/features/simple.py
@@ -0,0 +1,12 @@
+from features import FeatureGenerator
+
+
+class SimpleFeatureGenerator(FeatureGenerator):
+    def generate(self, dataset):
+        template = (-2, -1, 0, 1, 2)
+
+        for sentence in dataset.sentences():
+            for index, token in enumerate(sentence):
+                for template_index in template:
+                    if -1 < index + template_index < len(sentence):
+                        token.features['w[%d]' % template_index] = sentence[index+template_index]
--- source/main.py
+++ source/main.py
@@ -2,6 +2,8 @@ from utils.readers import HTMLReader
 from preprocessing.spliters import NTLKSplitter
 from preprocessing.tokenizers import NLTKTokenizer
 from preprocessing.annotators import ReadFromAnnJsonAnnotator
+from features.simple import SimpleFeatureGenerator
+
 
 if __name__ == "__main__":
 
@@ -9,12 +11,19 @@ if __name__ == "__main__":
     ann_path = r'C:\Users\Aleksandar\Desktop\Json and Html\IDP4_members_json\pool\abojchevski'
 
     dataset = HTMLReader(path).read()
+
     NTLKSplitter().split(dataset)
     NLTKTokenizer().tokenize(dataset)
+
     ReadFromAnnJsonAnnotator(ann_path).annotate(dataset)
+
+    SimpleFeatureGenerator().generate(dataset)
 
     for sentence in dataset.sentences():
         print(sentence)
+        for token in sentence:
+            print(token, token.features)
+        print('\n\n')
 
 
 

0ab2965053af1691a4c758266d2935c83ce837ea
Aleksandar Bojchevski <aleksandar.bojchevski@gmail.com>
2015-05-24 19:55:56 +0200

    [#19 Set of regexs] - Fixed first couple regexes

--- source/utils/regex_count.py
+++ source/utils/regex_count.py
@@ -9,14 +9,22 @@ def count_matches(dataset):
 
     matches = []
 
+    for regex in regexes:
+        if regex[0].startswith('(?-xism:'):
+            try:
+                regex[0] = re.compile(regex[0].replace('(?-xism:', ''),
+                                      re.VERBOSE | re.IGNORECASE | re.DOTALL | re.MULTILINE)
+            except:
+                regex[0] = None
+        else:
+            regex[0] = re.compile(regex[0])
+
+
     for part in dataset.parts():
         for ann in part.annotations:
             for regex in regexes:
-                try:
-                    if re.match(regex[0],ann.text):
-                        matches.append(regex[1])
-                        break
-                except:
-                    pass
+                if regex[0] is not None and regex[0].match(ann.text):
+                    matches.append(regex[1])
+                    break
 
     print(Counter(matches))

a9c81b4dab4cb4fff231e92acd86aec6eef4ff03
Carsten Uhlig <carsten.uhlig@gmail.com>
2015-06-17 12:06:50 +0200

    #test commit something

--- nala/utils/writers.py
+++ nala/utils/writers.py
@@ -159,7 +159,6 @@ class StatsWriter:
 
         # subplot minimum one abstract/full
         # OPTIONAL subplot minimum one abstract/full
-        # test
 
         # plt.plot(annotate_array, nl_total_ratio_array, 'rs', annotate_array, abstract_full_ratio_array, 'bs')
         # plt.axis([self.init_counter, self.init_counter + total_counter - 1, 0, 3])

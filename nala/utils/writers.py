26fb75c8073654809cf5a8734b4cf8a8aeff9a97
Aleksandar Bojchevski <aleksandar.bojchevski@gmail.com>
2015-08-11 17:30:00 +0200

    [#60] Add console writer

--- nala/utils/writers.py
+++ nala/utils/writers.py
@@ -5,7 +5,8 @@ import matplotlib.pyplot as plt
 import math
 import xml.etree.ElementTree as ET
 import json
-
+import sys
+import os
 
 class StatsWriter:
     """ Is able to be populated by stats and then be exported into various formats.
@@ -311,3 +312,58 @@ class TagTogFormat:
                         json_obj['entities'].append(ent)
 
                 json.dump(json_obj, f)
+
+
+class ConsoleWriter:
+    """
+    Writes the predicted annotations onto the console.
+    """
+    def __init__(self):
+        self.color = self.__supports_color()
+        self.which_color = '\033[42m'
+        self.end_color = '\033[0m'
+        pass
+
+    def write(self, dataset):
+        """
+        :type dataset: nala.structures.data.Dataset()
+        """
+        for doc_id, doc in dataset.documents.items():
+            print('DOCUMENT: {}'.format(doc_id))
+            for part_id, part in doc.parts.items():
+                print('PART {}'.format(part_id))
+                self.___print_part(part)
+
+    def ___print_part(self, part):
+        if self.color:
+            text = part.text
+            total_offset = 0
+            for ann in part.predicted_annotations:
+                text = text[:ann.offset+total_offset] + self.which_color + text[ann.offset+total_offset:]
+                total_offset += 5
+                text = text[:ann.offset+len(ann.text)+total_offset] + self.end_color + text[ann.offset+len(ann.text)+total_offset:]
+                total_offset += 4
+                print(ann.text.upper())
+            print(text)
+            print()
+        else:
+            padding = len(str(len(part.text)))
+            print(part.text)
+            print('ANNOTATIONS')
+            for ann in part.predicted_annotations:
+                print('{0: <{pad}} {1: <{pad}} {2}'.format(ann.offset, ann.offset+len(ann.text), ann.text, pad=padding))
+            print()
+
+    @staticmethod
+    def __supports_color():
+        """
+        Returns True if the running system's terminal supports color, and False
+        otherwise.
+        """
+        plat = sys.platform
+        supported_platform = plat != 'Pocket PC' and (plat != 'win32' or 'ANSICON' in os.environ)
+        is_a_tty = hasattr(sys.stdout, 'isatty') and sys.stdout.isatty()
+
+        if not supported_platform or not is_a_tty:
+            return False
+        return True

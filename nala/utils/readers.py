b66954a10aa68d7cfca739c9af6f54b76fa57a93
Carsten Uhlig <carsten.uhlig@gmail.com>
2015-08-11 20:41:51 +0200

    [#37] change id naming to s1pIDoldid

--- nala/utils/readers.py
+++ nala/utils/readers.py
@@ -124,6 +124,12 @@ class VerspoorReader(Reader):
         """
         dataset = Dataset()
         file_list = glob.glob(str(self.directory + "/*.txt"))
+
+        # last pmid
+        last_pmid = ""
+        id_counter = 0
+        ids_per_file_array = [1]
+
         for file_path in file_list:
             file_name = os.path.basename(file_path)
 
@@ -141,6 +147,14 @@ class VerspoorReader(Reader):
             text = text_raw.replace('** IGNORE LINE **\n', '')
             paragraph_list = text.split('\n\n')
 
+            # check for last document the same as now, otherwise restart counter for ids
+            if pmid != last_pmid:
+                id_counter = 0
+                ids_per_file_array = [1]
+                last_pmid = pmid
+            else:
+                ids_per_file_array.append(id_counter + 1)  # id_counter finishes with last id of last file so --> +1
+
             # inital offset for raw_text
             tot_offset = text_raw.count('** IGNORE LINE **\n') * 18
             offsets = [tot_offset]
@@ -148,24 +162,28 @@ class VerspoorReader(Reader):
             for i, text_part in enumerate(paragraph_list):
                 # if text is empty (usually last text due to splitting of "\n\n")
                 if text_part != "":
-                    pass
                     first_serial = "s" + serial
                     second_serial = "s" + paragraph.replace("p", "")
 
+                    # simple_id counter starts with 0 and before each use in simple_id var increment 1
+                    id_counter += 1
+
                     # OPTIONAL to activate but annotation reader has to modified as well
                     # header when 10 >= words in text_part
-                    # if len(text_part.split(" ")) < 10:
-                    #     paragraph_id = "h" + "{:02d}".format(i + 1)
-                    # else:
-                    paragraph_id = "p" + "{:02d}".format(i + 1)
+                    if len(text_part.split(" ")) < 10:
+                        paragraph_id = "h" + "{:02d}".format(i + 1)
+                        simple_id = "s1h{}".format(id_counter)
+                    else:
+                        paragraph_id = "p" + "{:02d}".format(i + 1)
+                        simple_id = "s1p{}".format(id_counter)
 
                     partid = "{0}{1}{2}".format(first_serial, second_serial, paragraph_id)
 
                     if pmid in dataset:
-                        dataset.documents[pmid].parts[partid] = Part(text_part)
+                        dataset.documents[pmid].parts[simple_id + partid] = Part(text_part)
                     else:
                         document = Document()
-                        document.parts[partid] = Part(text_part)
+                        document.parts[simple_id + partid] = Part(text_part)
                         dataset.documents[pmid] = document
 
                     # add offset for next paragraph
@@ -192,37 +210,60 @@ class VerspoorReader(Reader):
                         for i, off in enumerate(offsets):
                             if int(start) < off:
                                 id = "p{:02d}".format(i)
+                                # id_h = "h{:02d}".format(i)
+
                                 to_correct_off = last_off
+
+                                simple_id = "s1p{}".format(ids_per_file_array[-1] + i - 1)  # -1 since starting to count with 1
+                                # simple_id_h = "s1h{}".format(ids_per_file_array[-1] + i - 1)
                                 break
                             last_off = off
 
                         # if last element
                         if id == "None":
                             id = "p{:02d}".format(len(offsets))
+
+                            simple_id = "s1p{}".format(ids_per_file_array[-1] + len(offsets) - 1)
+                            # simple_id_h = "s1h{}".format(ids_per_file_array[-1] + len(offsets) - 1)
+
                             to_correct_off = offsets[-1]
 
                         properid = "{0}{1}{2}".format(first_serial, second_serial, id)
+                        # properid_h = "{0}{1}{2}".format(first_serial, second_serial, id_h)
 
                         if id == "None":
                             print("None???", pmid, serial, paragraph, start, offsets)
 
-                        if (first_serial + second_serial + id) not in dataset.documents[pmid].parts:
-                            print("NoKEY???", dataset.documents[pmid].parts)
+                        # if (simple_id + properid) not in dataset.documents[pmid].parts or (simple_id.replace("p", "h") + properid.replace("p", "h")) not in dataset.documents[pmid].parts:
+                        #     print("NoKEY???", dataset.documents[pmid].parts)
                             # print(json.dumps(document, indent=4))
 
                         # print(document.parts[properid].text[int(start) - to_correct_off:int(start) - to_correct_off + len(row[2])], "==", row[2])
 
+                        try:
+                            calc_ann_text = document.parts[simple_id + properid].text[int(start) - to_correct_off:int(start) - to_correct_off + len(row[2])]
+                        except KeyError:
+                            simple_id = simple_id.replace("p", "h")
+                            properid = properid.replace("p", "h")
+                            calc_ann_text = document.parts[simple_id + properid].text[int(start) - to_correct_off:int(start) - to_correct_off + len(row[2])]
 
-                        calc_ann_text = document.parts[properid].text[int(start) - to_correct_off:int(start) - to_correct_off + len(row[2])]
+
                         if calc_ann_text != row[2]:
                             print(pmid, serial, paragraph, start, to_correct_off, id)
 
                         if entity_type == 'mutation':
                             ann = Annotation('e_2', int(start) - to_correct_off, row[2])
-                            dataset.documents[pmid].parts[properid].annotations.append(ann)
+                            # try:
+                            dataset.documents[pmid].parts[simple_id + properid].annotations.append(ann)
+                            # except KeyError:
+                            #     dataset.documents[pmid].parts[simple_id_h + properid_h].annotations.append(ann)
+
                         elif entity_type == 'gene':
                             ann = Annotation('e_1', int(start) - to_correct_off, row[2])
-                            dataset.documents[pmid].parts[properid].annotations.append(ann)
+                            # try:
+                            dataset.documents[pmid].parts[simple_id + properid].annotations.append(ann)
+                            # except KeyError:
+                            #     dataset.documents[pmid].parts[simple_id_h + properid_h].annotations.append(ann)
 
         return dataset
 

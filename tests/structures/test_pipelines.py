8882e32814a68535c621af971cf344a9831becc6
Carsten Uhlig <carsten.uhlig@gmail.com>
2015-08-19 00:20:50 +0200

    [#70] structures tests

---
+++ tests/structures/test_pipelines.py
@@ -0,0 +1,11 @@
+from unittest import TestCase
+
+__author__ = 'carsten'
+
+
+class TestPrepareDatasetPipeline(TestCase):
+    def test_execute(self):
+        self.fail()
+
+    def test_init(self):
+        self.fail()

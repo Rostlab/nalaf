2a711b0d320f46c3101101f903161426ac849641
Carsten Uhlig <carsten.uhlig@gmail.com>
2015-08-19 00:34:17 +0200

    [#70] restructure features package

---
+++ tests/features/test_stemming.py
@@ -0,0 +1,8 @@
+from unittest import TestCase
+
+__author__ = 'carsten'
+
+
+class TestPorterStemFeatureGenerator(TestCase):
+  def test_generate(self):
+    self.fail()
---
+++ tests/features/test_window.py
@@ -0,0 +1,8 @@
+import unittest
+
+class TestWindowFeatureGenerator(unittest.TestCase):
+    def test_init(self):
+        self.fail()
+
+    def test_generate(self):
+        self.fail()

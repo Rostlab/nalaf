91cef8ab16951a2ade4b31e88ff33318b031351c
Carsten Uhlig <carsten.uhlig@gmail.com>
2015-08-19 00:20:50 +0200

    [#70] structures tests

---
+++ tests/structures/test_data.py
@@ -0,0 +1,89 @@
+from unittest import TestCase
+
+__author__ = 'carsten'
+
+
+class TestDataset(TestCase):
+    def test_parts(self):
+        self.fail()
+
+    def test_annotations(self):
+        self.fail()
+
+    def test_predicted_annotations(self):
+        self.fail()
+
+    def test_sentences(self):
+        self.fail()
+
+    def test_tokens(self):
+        self.fail()
+
+    def test_partids_with_parts(self):
+        self.fail()
+
+    def test_annotations_with_partids(self):
+        self.fail()
+
+    def test_all_annotations_with_ids(self):
+        self.fail()
+
+    def test_form_predicted_annotations(self):
+        self.fail()
+
+    def test_clean_nl_definitions(self):
+        self.fail()
+
+    def test_stats(self):
+        self.fail()
+
+
+class TestDocument(TestCase):
+    def test_key_value_parts(self):
+        self.fail()
+
+    def test_get_unique_mentions(self):
+        self.fail()
+
+    def test_get_size(self):
+        self.fail()
+
+
+class TestToken(TestCase):
+    def test_init(self):
+        self.fail()
+
+    def test_repr(self):
+        self.fail()
+
+
+class TestFeatureDictionary(TestCase):
+    def test_setitem(self):
+        self.fail()
+
+
+class TestAnnotation(TestCase):
+    def test_init(self):
+        self.fail()
+
+    def test_repr(self):
+        self.fail()
+
+    def test_eq(self):
+        self.fail()
+
+
+class TestLabel(TestCase):
+    def test_repr(self):
+        self.fail()
+
+    def test_init(self):
+        self.fail()
+
+
+class TestPart(TestCase):
+    def test_init(self):
+        self.fail()
+
+    def test_iter(self):
+        self.fail()

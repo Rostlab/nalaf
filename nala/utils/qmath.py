279b5233526ae99eccb080f62d001b46aaa320ed
Carsten Uhlig <carsten.uhlig@gmail.com>
2015-08-19 17:30:07 +0200

    [#74] bugfix qmath

--- nala/utils/qmath.py
+++ nala/utils/qmath.py
@@ -6,8 +6,9 @@ which does have those functions implemented.
 # todo implement test functions
 
 
-def mean(arr_of_nr):
+def arithmetic_mean(arr_of_nr):
     """
+    arithmetic mean
     :param arr_of_nr: array of real numbers
     :return: average of array
     """
@@ -15,9 +16,9 @@ def mean(arr_of_nr):
         raise IndexError('no element in array')
     # if not any(isinstance(x, (int, float)) for x in arr_of_nr):
     #     raise TypeError
-    return sum(isinstance(x, (float, int)) for x in arr_of_nr)/len(arr_of_nr)
+    return sum(x for x in arr_of_nr if isinstance(x, (float, int)))/len(arr_of_nr)
 
-def hmean(arr_of_pos_nr):
+def harmonic_mean(arr_of_pos_nr):
     """
     harmonic mean
     :param arr_of_pos_nr: array of pos real numbers
@@ -27,4 +28,4 @@ def hmean(arr_of_pos_nr):
         raise IndexError('no element in array')
     # if not any(isinstance(x, (int, float)) and x >= 0 for x in arr_of_pos_nr):
     #     raise TypeError
-    return len(arr_of_pos_nr) / sum(isinstance(x, (float, int)) and 1/x for x in arr_of_pos_nr)
+    return len(arr_of_pos_nr) / sum(1/x for x in arr_of_pos_nr if isinstance(x, (float, int)))

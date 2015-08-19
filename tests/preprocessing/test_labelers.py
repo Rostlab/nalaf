0ec578b05af256d92f98e7620d0cc82dc5915bef
Carsten Uhlig <carsten.uhlig@gmail.com>
2015-08-19 02:54:11 +0200

    [#70] restructure test classes

---
+++ tests/preprocessing/test_labelers.py
@@ -0,0 +1,30 @@
+import unittest
+
+
+class TestLabeler(unittest.TestCase):
+    def test_label(self):
+        self.fail()  # TODO
+
+
+class TestBIOLabeler(unittest.TestCase):
+    def test_label(self):
+        self.fail()  # TODO
+
+
+class TestTmVarLabeler(unittest.TestCase):
+    def test_init(self):
+        self.fail()  # TODO
+
+    def test_match_regex_label(self):
+        self.fail()  # TODO
+
+    def test_label(self):
+        self.fail()  # TODO
+
+
+class TestBIEOLabeler(unittest.TestCase):
+    def test_label(self):
+        self.fail()  # TODO
+
+if __name__ == '__main__':
+    unittest.main()

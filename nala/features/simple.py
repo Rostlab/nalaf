7c0aff9cf464f5844f64cf50d71e482ebd7b9607
Aleksandar Bojchevski <aleksandar.bojchevski@gmail.com>
2015-08-24 12:42:04 +0200

    [#45] Cleanup

--- nala/features/simple.py
+++ nala/features/simple.py
@@ -15,3 +15,21 @@ class SimpleFeatureGenerator(FeatureGenerator):
         """
         for token in dataset.tokens():
             token.features['word'] = token.word
+
+
+class SentenceMarkerFeatureGenerator(FeatureGenerator):
+    """
+    Generates BOS and EOS features
+        * BOS[0] = is it at the beginning of a sentence?
+        * EOS[0] = is it at the end of a sentence?
+
+    Implements the abstract class FeatureGenerator.
+    """
+
+    def generate(self, dataset):
+        """
+        :type dataset: nala.structures.data.Dataset
+        """
+        for sentence in dataset.sentences():
+            sentence[0].features['BOS'] = 1
+            sentence[-1].features['EOS'] = 1

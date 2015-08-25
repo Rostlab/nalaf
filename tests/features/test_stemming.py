4fb6e7736267d62d90048bb4a5ca8e21522e7830
Aleksandar Bojchevski <aleksandar.bojchevski@gmail.com>
2015-08-25 16:26:10 +0200

    [#60] Add test for PorterStemFeatureGenerator

--- tests/features/test_stemming.py
+++ tests/features/test_stemming.py
@@ -1,9 +1,27 @@
 import unittest
+from nala.structures.data import Dataset, Document, Part, Token
+from nala.features.stemming import PorterStemFeatureGenerator
 
 
 class TestPorterStemFeatureGenerator(unittest.TestCase):
+    def setUp(self):
+        part = Part('Make making made. Try tried tries.')
+        part.sentences = [[Token('Make'), Token('making'), Token('made')],
+                          [Token('try'), Token('tried'), Token('tries')]]
+
+        self.dataset = Dataset()
+        self.dataset.documents['doc_1'] = Document()
+        self.dataset.documents['doc_1'].parts['part_1'] = part
+
+        self.generator = PorterStemFeatureGenerator()
+
     def test_generate(self):
-        pass  # TODO
+        self.generator.generate(self.dataset)
+        features = [token.features for token in self.dataset.tokens()]
+        expected = iter([{'stem[0]': 'Make'}, {'stem[0]': 'make'}, {'stem[0]': 'made'},
+                         {'stem[0]': 'tri'}, {'stem[0]': 'tri'}, {'stem[0]': 'tri'}])
+        for feature in features:
+            self.assertEqual(feature, next(expected))
 
 
 if __name__ == '__main__':

34ff2d4a82c7d10dc52e0d4a2ad6d2ef25cac9f3
Aleksandar Bojchevski <aleksandar.bojchevski@gmail.com>
2015-09-10 12:43:04 +0200

    Add caching to GNormPlus

--- nala/utils/ncbi_utils.py
+++ nala/utils/ncbi_utils.py
@@ -1,4 +1,6 @@
 import requests
+import json
+import os
 
 
 class GNormPlus:
@@ -8,12 +10,25 @@ class GNormPlus:
     """
     def __init__(self):
         self.url = 'http://www.ncbi.nlm.nih.gov/CBBresearch/Lu/Demo/RESTful/tmTool.cgi/Gene/{}/PubTator/'
+        if os.path.exists('gnp_cache.json'):
+            self.cache = json.load(open('gnp_cache.json'))
+        else:
+            self.cache = {}
+
+    def __del__(self):
+        with open('gnp_cache.json', 'w') as file:
+            json.dump(self.cache, file)
 
     def get_genes_for_pmid(self, pmid):
-        req = requests.get(self.url.format(pmid))
+        if pmid in self.cache:
+            text = self.cache[pmid]
+        else:
+            req = requests.get(self.url.format(pmid))
+            text = req.text
+            self.cache[pmid] = text
 
         genes = []
-        for line in req.text.splitlines()[2:-1]:  # skip title and abstract
+        for line in text.splitlines()[2:-1]:  # skip title and abstract
             _, start, end, text, _, gene_id = line.split('\t')
-            genes.append((start, end, text, gene_id))
+            genes.append((int(start), int(end), text, gene_id))
         return genes

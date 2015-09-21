0ba50adb137b1c14891a21c00092fa37f6028ba5
Carsten Uhlig <carsten.uhlig@gmail.com>
2015-09-21 02:06:19 +0200

    [#88] tagger class init

--- nala/bootstrapping/document_filters.py
+++ nala/bootstrapping/document_filters.py
@@ -1,4 +1,10 @@
 import abc
+import re
+import time
+from preprocessing.definers import InclusiveNLDefiner
+from preprocessing.definers import ExclusiveNLDefiner
+from structures.data import Dataset
+from utils.readers import TmVarReader
 
 
 class DocumentFilter:
@@ -45,3 +51,131 @@ class KeywordsDocumentFilter(DocumentFilter):
                 yield pmid, doc
 
 
+class HighRecallRegexDocumentFilter(DocumentFilter):
+    """
+    Filter that uses regular expression to first get possible natural language mentions in sentences.
+    Then each possible nl mention gets compared to tmVar results and Nala predictions. If there is no overlap,
+    then this annotation will be considered as nl mention and thus the document will not be filtered out.
+    Ever other document gets filtered out at this point.
+
+    Condition for being filtered out:
+    Not any(sentence that contains valid nl mention according to this definition)
+
+    tmVar will be used in early stages and discarded as soon as there are no more results, thus gets a parameter.
+    """
+    def __init__(self, binary_model="", override_cache=False, expected_max_results=5):
+        self.location_binary_model = binary_model
+        """ location where binary model for nala (crfsuite) is saved """
+        self.override_cache=override_cache
+        """ tmvar results are saved in cache and reused from there.
+        this option allows to force requesting results from tmVar online """
+        self.expected_maximum_results=expected_max_results
+        """ :returns maximum of [x] documents (can be less if not found) """
+
+    def filter(self, documents):
+        """
+        :type documents: collections.Iterable[(str, nala.structures.data.Document)]
+        """
+
+        dataset = Dataset()
+        for pmid, doc in documents:
+            dataset.documents[pmid] = doc
+
+        # dataset with tmVar
+        data_tmvar = TmVarReader('resources/corpora/idp4/pubtator_tmvar.txt').read()
+        TP = 0
+        FP = 0
+        _length = len(dataset.documents.keys())
+        _progress = 0
+        _timestart = time.time()
+
+        _time_avg_per_pattern = 0
+        _pattern_calls = 0
+        _time_reg_pattern_total = 0
+        _time_max_pattern = 0
+        _low_performant_pattern = ""
+        _avg_chars_per_doc = dataset.get_size_chars() / len(dataset.documents.keys())
+
+        # NLDefiners init
+        exclusive_definer = ExclusiveNLDefiner()
+        _e_array = [0, 0, 0]
+        inclusive_definer = InclusiveNLDefiner()
+        _i_array = [0, 0]
+        for pmid, doc in documents:
+            # if any part of the document contains any of the keywords
+            # yield that document
+            # TODO nltk include for each sentence
+            part_offset = 0
+            for i, x in enumerate(doc.parts):
+                # print("Part", i)
+                sent_offset = 0
+                cur_part = doc.parts.get(x)
+                sentences = cur_part.sentences
+                # new_text = cur_part.text.lower()
+                # new_text = re.sub('\s+', ' ', new_text)
+                # sentences = new_text.split('. ')
+                for sent in sentences:
+                    part_length = len(sent)
+                    new_text = sent.lower()
+                    new_text = re.sub('[\./\\-(){}\[\],%]', '', new_text)
+                    new_text = re.sub('\W+', ' ', new_text)
+                    for i, reg in enumerate(patterns):
+
+                        _lasttime = time.time()  # time start var
+                        match = reg.search(new_text)
+
+                        # debug bottleneck patterns
+                        _time_current_reg = time.time() - _lasttime  # time end var
+                        _pattern_calls += 1  # pattern calls already occured
+                        _time_reg_pattern_total += _time_current_reg  # total time spent on searching with patterns
+                        if _time_reg_pattern_total > 0:
+                            _time_avg_per_pattern = _time_reg_pattern_total / _pattern_calls  # avg spent time per pattern call
+
+                        # if _pattern_calls > len(patterns) * 20 and _time_avg_per_pattern * 10000 < _time_current_reg:
+                        #     print("BAD_PATTERN_PERFORMANCE:", _time_avg_per_pattern, _time_current_reg, reg.pattern)
+                        # if _time_max_pattern < _time_current_reg:
+                        #     _time_max_pattern = _time_current_reg
+                        #     _low_performant_pattern = reg.pattern
+                        #     print(_time_avg_per_pattern, _low_performant_pattern, _time_max_pattern)
+
+                        # if reg.pattern == r'(\b\w*\d+\w*\b\s?){1,3} (\b\w+\b\s?){1,4} (\b\w*\d+\w*\b\s?){1,3} (\b\w+\b\s?){1,4} (deletion|deleting|deleted)':
+                        #     if _time_current_reg > _time_avg_per_pattern * 10:
+                        #         # print(_time_avg_per_pattern, _time_current_reg)
+                        #         f.write("BAD_PATTERN\n")
+                        #         f.write(sent + "\n")
+                        #         f.write(new_text + "\n")
+
+                        from nala.structures.data import Annotation
+                        Annotation.equality_operator = 'exact_or_overlapping'
+                        if match:
+                            if did in data_tmvar.documents:
+                                anti_doc = data_tmvar.documents.get(did)
+                                if not anti_doc.overlaps_with_mention(match.span()):
+                                    _e_result = exclusive_definer.define_string(new_text[match.span()[0]:match.span()[1]])
+                                    _e_array[_e_result] += 1
+                                    _i_result = inclusive_definer.define_string(new_text[match.span()[0]:match.span()[1]])
+                                    _i_array[_i_result] += 1
+                                    if doc.overlaps_with_mention(match.span()):
+                                        TP += 1
+                                        f.write("TP\te{}\ti{}\t{}\t{}\t{}\n".format(_e_result, _i_result, sent, match, reg.pattern))
+                                        _perf_patterns[reg.pattern][0] += 1
+                                    else:
+                                        FP += 1
+                                        f.write("FP\te{}\ti{}\t{}\t{}\t{}\n".format(_e_result, _i_result, sent, match, reg.pattern))
+                                        _perf_patterns[reg.pattern][1] += 1
+
+                                    if _perf_patterns[reg.pattern][1] > 0:
+                                            _perf_patterns[reg.pattern][2] = _perf_patterns[reg.pattern][0] / _perf_patterns[reg.pattern][1]
+                                    break
+                        if _lasttime - time.time() > 1:
+                            print(i)
+                    sent_offset += 2 + part_length
+                part_offset += sent_offset
+            _progress += doc.get_size() / _avg_chars_per_doc
+            _time_progressed = time.time() - _timestart
+            _time_per_doc = _time_progressed / _progress
+            _time_req_time = _time_per_doc * _length
+            _time_eta = _time_req_time - _time_progressed
+            print("PROGRESS: {:.3%} PROGRESS: {:.2f} secs ETA: {:.2f} secs".format(_progress/_length, _time_progressed, _time_eta))
+            if TP + FP > 0:
+                print('STATS: TP:{}, FP:{}, TP+FP:{} %containingNLmentions:{:.4%}'.format(TP, FP, TP+FP, TP/(TP + FP)))

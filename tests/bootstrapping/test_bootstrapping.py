b652593ed323691a55668f47c3d68cce193ad9b4
Aleksandar Bojchevski <aleksandar.bojchevski@gmail.com>
2015-09-21 12:42:13 +0200

    Add caching for DocumentArticle

--- tests/bootstrapping/test_bootstrapping.py
+++ tests/bootstrapping/test_bootstrapping.py
@@ -5,8 +5,8 @@ from nala.bootstrapping import generate_documents
 class TestBootstrapping(unittest.TestCase):
     def test_generate_documents_number(self):
         # commenting out for now since it takes about 6 mins on Travis CI
-        # test_dataset = generate_documents(1)
-        # self.assertEqual(len(test_dataset), 1)
+        test_dataset = generate_documents(1)
+        self.assertEqual(len(test_dataset), 1)
         pass
 
 

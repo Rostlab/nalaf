ba2ec0652020e5467ba0a5001bb60db9821b094d
Aleksandar Bojchevski <aleksandar.bojchevski@gmail.com>
2015-09-22 13:14:41 +0200

    [#104] Don't run slow tests

--- tests/bootstrapping/test_bootstrapping.py
+++ tests/bootstrapping/test_bootstrapping.py
@@ -1,14 +1,15 @@
 import unittest
 from nala.bootstrapping import generate_documents
+from nose.plugins.attrib import attr
 
 
+@attr('slow')
 class TestBootstrapping(unittest.TestCase):
     def test_generate_documents_number(self):
         # commenting out for now since it takes about 6 mins on Travis CI
-        test_dataset = generate_documents(1)
-        # self.assertEqual(len(test_dataset), 1)
+        test_dataset = generate_documents(2)
+        self.assertEqual(len(test_dataset), 2)
         pass
-
 
 if __name__ == '__main__':
     unittest.main()

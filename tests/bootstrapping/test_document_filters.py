5e77d6596386429594bd624e7fcbbc873afe8176
Carsten Uhlig <carsten.uhlig@gmail.com>
2015-09-27 16:30:37 +0200

    #96 thresholdmodule ... just a parameter

--- tests/bootstrapping/test_document_filters.py
+++ tests/bootstrapping/test_document_filters.py
@@ -1,5 +1,6 @@
 import unittest
 from nose.plugins.attrib import attr
+from nala.bootstrapping.utils import generate_documents
 from nala.bootstrapping.document_filters import HighRecallRegexDocumentFilter
 from nala.structures.data import Dataset
 
@@ -11,30 +12,7 @@ class TestHighRecallRegexFilter(unittest.TestCase):
         pass
 
     def test_filter(self):
-        from nala.bootstrapping.utils import UniprotDocumentSelector
-        from nala.bootstrapping.document_filters import KeywordsDocumentFilter
-        from nala.bootstrapping.pmid_filters import AlreadyConsideredPMIDFilter
-        from nala.bootstrapping.utils import DownloadArticle
-        from itertools import count
-        c = count(1)
-
-        dataset = Dataset()
-
-        max_docs = 200
-
-        # use in context manager to enable caching
-        with UniprotDocumentSelector() as uds:
-            with DownloadArticle() as da:
-                for pmid, document in \
-                        HighRecallRegexDocumentFilter().filter(
-                            KeywordsDocumentFilter().filter(
-                                da.download(
-                                    AlreadyConsideredPMIDFilter('resources/bootstrapping', 4).filter(
-                                        uds.get_pubmed_ids()))), log_nr=max_docs):
-                    dataset.documents[pmid] = document
-                    # if we have generated enough documents stop
-                    if next(c) == max_docs:
-                        break
+        generate_documents(1)
 
 if __name__ == '__main__':
     unittest.main()

495400dd1d36da5cd4e5a4ec23ad6b14a86cc277
Carsten Uhlig <carsten.uhlig@gmail.com>
2015-09-27 20:50:47 +0200

    removed invalid imports for travis cl

--- tests/bootstrapping/test_iteration.py
+++ tests/bootstrapping/test_iteration.py
@@ -1,9 +1,7 @@
 from unittest import TestCase
 from nala.bootstrapping.iteration import Iteration
 from nose.plugins.attrib import attr
-from nala.utils.writers import TagTogFormat
 import os
-from utils.readers import HTMLReader
 
 
 @attr('slow')
@@ -18,9 +16,6 @@ class TestIteration(TestCase):
         # for ann in iteration.candidates.predicted_annotations():
         #     print(ann)
         # note current working directory changes after crfsuite to crfsuite-folder
-        # ttf = TagTogFormat(iteration.candidates, "/Users/carsten/tmp")
-        # ttf.export_html()
-        # ttf.export_ann_json(0.7)
 
         iteration.manual_review_import()
         iteration.evaluation()

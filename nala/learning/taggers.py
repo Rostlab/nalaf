bc1daec3b20d4141e5314b3380c4cbc04ed08eaa
Carsten Uhlig <carsten.uhlig@gmail.com>
2015-10-05 22:58:48 +0200

    #114 gnormplusgenetagger continue + todo save into parts --> indices make right

--- nala/learning/taggers.py
+++ nala/learning/taggers.py
@@ -1,4 +1,7 @@
 import abc
+import re
+from utils.ncbi_utils import GNormPlus
+from utils.uniprot_utils import Uniprot
 
 
 class Tagger:
@@ -62,3 +65,38 @@ class CRFSuiteMutationTagger(Tagger):
         self.crf_suite.create_input_file(dataset, 'predict')
         self.crf_suite.tag('-m {} -i predict > output.txt'.format(self.model_file))
         self.crf_suite.read_predictions(dataset)
+
+
+class GNormPlusGeneTagger(Tagger):
+    """
+    Performs tagging for genes with GNormPlus.
+    Is able to add normalisations for uniprot as well.
+
+    :type crf_suite: nala.learning.crfsuite.CRFSuite
+    """
+
+    def __init__(self, predicts_classes, normalise_uniprot=False):
+        super().__init__(predicts_classes)
+        self.uniprot=normalise_uniprot
+
+    def tag(self, dataset):
+        """
+        :type dataset: nala.structures.data.Dataset
+        """
+        with GNormPlus() as gnorm:
+            for docid, doc in dataset.documents.items():
+                if doc.get_text().contains('Conclusion'):
+                    genes = gnorm.get_genes_for_text(doc, docid, postproc=True)
+                else:
+                    genes = gnorm.get_genes_for_pmid(docid, postproc=True)
+
+                # genes
+                # if uniprot normalisation as well then:
+                genes_mapping = None
+                if self.uniprot:
+                    with Uniprot() as uprot:
+                        list_of_ids = gnorm.uniquify_genes(genes)
+                        genes_mapping = uprot.get_uniprotid_for_entrez_geneid(list_of_ids)
+
+                part_index = 0
+                last_part_index = -1

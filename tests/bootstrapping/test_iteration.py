98c174a4c7792785e280c86a2274aed57c506148
Carsten Uhlig <carsten.uhlig@gmail.com>
2015-10-07 13:43:39 +0200

    travis import bugfix

--- tests/bootstrapping/test_iteration.py
+++ tests/bootstrapping/test_iteration.py
@@ -1,10 +1,10 @@
 from unittest import TestCase
-from learning.taggers import GNormPlusGeneTagger, RelationshipExtractionGeneMutation
+from nala.learning.taggers import GNormPlusGeneTagger, RelationshipExtractionGeneMutation
 from nala.bootstrapping.iteration import Iteration
 from nose.plugins.attrib import attr
 import os
-from utils import PRO_REL_MUT_CLASS_ID, UNIPROT_ID, ENTREZ_GENE_ID
-from utils.writers import TagTogFormat
+from nala.utils import PRO_REL_MUT_CLASS_ID, UNIPROT_ID, ENTREZ_GENE_ID
+from nala.utils.writers import TagTogFormat
 
 
 @attr('slow')

8978cd7b79cb9f277043dbf7482735a6e4c6ffa3
Aleksandar Bojchevski <aleksandar.bojchevski@gmail.com>
2016-02-04 09:14:29 +0100

    Make evaluator handle non matching overlapping subclasses

--- nalaf/learning/evaluators.py
+++ nalaf/learning/evaluators.py
@@ -95,7 +95,11 @@ class MentionLevelEvaluator(Evaluator):
                             overlap_real.append(ann_a)
                             overlap_predicted.append(ann_b)
 
-                            if self.subclass_analysis and ann_a.subclass == ann_b.subclass:
+                            if ann_a.subclass != ann_b.subclass:
+                                print_debug('overlapping subclasses do not match', ann_a.subclass, ann_b.subclass)
+                                ann_b.subclass = ann_a.subclass
+
+                            if self.subclass_analysis:
                                 overlap_subclass_real[ann_a.subclass].append(ann_a)
                                 overlap_subclass_predicted[ann_b.subclass].append(ann_b)
 

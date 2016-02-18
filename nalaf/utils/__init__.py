06cd0cd1ea3ed8d93345346b66dc59ca1afad8cc
Juan Miguel Cejuela <juanmi@jmcejuela.com>
2016-02-18 05:38:10 +0100

    mark TODO and make clear that constants are deprecated

--- nalaf/utils/__init__.py
+++ nalaf/utils/__init__.py
@@ -1,8 +1,11 @@
+THRESHOLD_VALUE = 1
+
+# TODO All the following constants are deprecated in nalaf and will soon be deleted
+
 PRO_CLASS_ID = 'e_1'
 MUT_CLASS_ID = 'e_2'
 ORG_CLASS_ID = 'e_3'
 PRO_REL_MUT_CLASS_ID = 'r_4'  # e_1/e_2
 PRO_REL_ORG_CLASS_ID = 'r_5'  # e_1/e_3
-THRESHOLD_VALUE = 1
 ENTREZ_GENE_ID = 'n_4'
 UNIPROT_ID = 'n_5'

c385a5fb96ee188f30ed8b75c35cbb05b8b84df1
Juan Miguel Cejuela <i@juanmi.rocks>
2016-04-11 18:23:26 +0200

    simplify current code in order to calculate SE later

--- nalaf/learning/evaluators.py
+++ nalaf/learning/evaluators.py
@@ -60,7 +60,7 @@ class Evaluation:
     def __computation_to_list(self, d):
         return [d.precision, d.recall, d.f_measure]
 
-    def format_header(self, strictnesses=None, add_SE=True):
+    def format_header(self, strictnesses=None, add_SE='macro'):
         strictnesses = ['exact', 'overlapping'] if strictnesses is None else strictnesses
 
         header = ['#class', 'tp', 'fp', 'fn', 'fp_ov', 'fn_ov']
@@ -68,8 +68,10 @@ class Evaluation:
             header += ['match', 'P', 'R', 'F']
         return '\t'.join(header)
 
-    def format(self, strictnesses=None, add_SE=True):
+    def format(self, strictnesses=None, add_SE='macro'):
         strictnesses = ['exact', 'overlapping'] if strictnesses is None else strictnesses
+
+        # TODO plus minus
 
         l = [self.label]
         counts = [self.tp, self.fp, self.fn, self.fp_ov, self.fn_ov]
@@ -105,7 +107,9 @@ class Evaluations:
     def __str__(self):
         return self.format()
 
-    def format(self, strictnesses=['exact', 'overlapping']):
+    def format(self, strictnesses=None, add_SE='macro'):
+        strictnesses = ['exact', 'overlapping'] if strictnesses is None else strictnesses
+
         assert(len(self.classes) >= 1)
         l = [next(iter(self.classes.values())).format_header(strictnesses)]
         for clazz in sorted(self.classes.keys()):
@@ -138,6 +142,9 @@ class Evaluator:
 
 
 class MentionLevelEvaluator(Evaluator):
+
+    TOTAL_LABEL = 'TOTAL'
+
     """
     Implements mention level performance evaluation. That means it compares if the predicted text spans match
     the original annotated text spans.
@@ -166,77 +173,78 @@ class MentionLevelEvaluator(Evaluator):
                 or in other words tp / tp + fn
             * possibly considers overlapping matches as well
         """
-        tp, fp, fn, fp_ov, fn_ov = 0, 0, 0, 0, 0
+
+        TOTAL = MentionLevelEvaluator.TOTAL_LABEL
+        labels = [TOTAL]
 
         if self.subclass_analysis:
             # find all possible subclasses
-            subclasses = set(ann.subclass for ann in dataset.annotations())
-            subclasses.update(set(ann.subclass for ann in dataset.predicted_annotations()))
-            # initialize counts to zero for each subclass
-            subclass_counts = {subclass: dict.fromkeys(['tp', 'fp', 'fn', 'fp_ov', 'fn_ov'], 0)
-                               for subclass in subclasses}
+            subclasses = set(ann.subclass for ann in dataset.annotations() if ann.subclass is not None)
+            subclasses.update(set(ann.subclass for ann in dataset.predicted_annotations() if ann.subclass is not None))
+            for x in subclasses:
+                labels.append(x)
 
-        for doc in dataset:
+        docids = dataset.documents.keys()
+        subcounts = ['tp', 'fp', 'fn', 'fp_ov', 'fn_ov']
+        counts = {label: {docid: dict.fromkeys(subcounts, 0) for docid in docids} for label in labels}
+
+        for docid, doc in dataset.documents.items():
             for part in doc:
                 print_debug(' || '.join(ann.text for ann in part.annotations))
                 print_debug(' || '.join(ann.text for ann in part.predicted_annotations))
                 print_debug()
 
-                overlap_real = []
-                overlap_predicted = []
-
-                if self.subclass_analysis:
-                    overlap_subclass_real = {subclass: [] for subclass in subclasses}
-                    overlap_subclass_predicted = {subclass: [] for subclass in subclasses}
+                overlap_real = {label: [] for label in labels}
+                overlap_predicted = {label: [] for label in labels}
 
                 Entity.equality_operator = 'overlapping'
                 for ann_a in part.annotations:
                     for ann_b in part.predicted_annotations:
-                        if ann_a == ann_b:
-                            overlap_real.append(ann_a)
-                            overlap_predicted.append(ann_b)
+                        if ann_a == ann_b:  # equal according according to exclusive overlapping eq (not exact)
+                            overlap_real[TOTAL].append(ann_a)
+                            overlap_predicted[TOTAL].append(ann_b)
 
                             if self.subclass_analysis:
                                 if ann_a.subclass != ann_b.subclass:
                                     print_debug('overlapping subclasses do not match', ann_a.subclass, ann_b.subclass)
                                     ann_b.subclass = ann_a.subclass
 
-                                overlap_subclass_real[ann_a.subclass].append(ann_a)
-                                overlap_subclass_predicted[ann_b.subclass].append(ann_b)
+                                overlap_real[ann_a.subclass].append(ann_a)
+                                overlap_predicted[ann_b.subclass].append(ann_b)
 
                 Entity.equality_operator = 'exact'
                 for ann in part.predicted_annotations:
                     if ann in part.annotations:
-                        tp += 1
+                        counts[TOTAL][docid]['tp'] += 1
                         if self.subclass_analysis:
-                            subclass_counts[ann.subclass]['tp'] += 1
+                            counts[ann.subclass][docid]['tp'] += 1
                     else:
-                        fp += 1
-                        if ann in overlap_predicted:
-                            fp_ov += 1
+                        counts[TOTAL][docid]['fp'] += 1
+                        if ann in overlap_predicted[TOTAL]:
+                            counts[TOTAL][docid]['fp_ov'] += 1
                         if self.subclass_analysis:
-                            subclass_counts[ann.subclass]['fp'] += 1
-                            if ann in overlap_subclass_predicted[ann.subclass]:
-                                subclass_counts[ann.subclass]['fp_ov'] += 1
+                            counts[ann.subclass][docid]['fp'] += 1
+                            if ann in overlap_predicted[ann.subclass]:
+                                counts[ann.subclass][docid]['fp_ov'] += 1
 
                 for ann in part.annotations:
                     if ann not in part.predicted_annotations:
-                        fn += 1
-                        if ann in overlap_real:
-                            fn_ov += 1
+                        counts[TOTAL][docid]['fn'] += 1
+                        if ann in overlap_real[TOTAL]:
+                            counts[TOTAL][docid]['fn_ov'] += 1
                         if self.subclass_analysis:
-                            subclass_counts[ann.subclass]['fn'] += 1
-                            if ann in overlap_subclass_real[ann.subclass]:
-                                subclass_counts[ann.subclass]['fn_ov'] += 1
+                            counts[ann.subclass][docid]['fn'] += 1
+                            if ann in overlap_real[ann.subclass]:
+                                counts[ann.subclass][docid]['fn_ov'] += 1
 
         evaluations = Evaluations()
 
-        if self.subclass_analysis:
-            for subclass, counts in subclass_counts.items():
-                if subclass is None:
-                    break
-                evaluations.add(Evaluation(str(subclass), counts['tp'], counts['fp'], counts['fn'], counts['fp_ov'], counts['fn_ov']))
-
-        evaluations.add(Evaluation('TOTAL', tp, fp, fn, fp_ov, fn_ov))
+        for label in labels:
+            evaluations.add(Evaluation(str(label),
+                                       sum([doc['tp'] for doc in counts[label].values()]),
+                                       sum([doc['fp'] for doc in counts[label].values()]),
+                                       sum([doc['fn'] for doc in counts[label].values()]),
+                                       sum([doc['fp_ov'] for doc in counts[label].values()]),
+                                       sum([doc['fn_ov'] for doc in counts[label].values()])))
 
         return evaluations

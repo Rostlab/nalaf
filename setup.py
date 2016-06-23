1f0b5c7eadb5d7abc96c76e68ebad8893ff26a49
Juan Miguel Cejuela <i@juanmi.rocks>
2016-06-23 18:47:35 +0200

    remove textblob dependency

--- setup.py
+++ setup.py
@@ -37,9 +37,6 @@ setup(
 
         # 'spacy'
         # Note: it may cause problems on different environments (e.g. Travis CI)-- install it manually
-
-        # 'textblob'
-        # Note: it may cause problems on different environments (e.g. Travis CI)-- install it manually
     ],
     include_package_data=True,
     zip_safe=False,

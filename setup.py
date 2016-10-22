199672334d91a9d9f289878fe0fb97dd2e8153c8
Juan Miguel Cejuela <i@juanmi.rocks>
2016-10-22 16:53:52 +0200

    include progress lib

--- setup.py
+++ setup.py
@@ -32,6 +32,8 @@ setup(
         'beautifulsoup4',
         'requests>=2.8.1',
         'python-crfsuite>=0.8.4',
+        'progress',
+
         # Note: it may cause problems on Windows machines
         # Throubleshooting
         # * Install python3-devel package or similar from your UNIX distribution

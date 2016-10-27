1a45bafb506d52180d97f9b988bd98e040eba43c
Juan Miguel Cejuela <i@juanmi.rocks>
2016-10-27 11:47:10 +0200

    minor

--- nalaf/features/relations.py
+++ nalaf/features/relations.py
@@ -2,6 +2,7 @@ import abc
 from nalaf.features import FeatureGenerator
 from nltk.stem import PorterStemmer
 import re
+from nalaf import print_debug
 
 
 class EdgeFeatureGenerator(FeatureGenerator):
@@ -64,17 +65,17 @@ class NamedEntityCountFeatureGenerator(EdgeFeatureGenerator):
     Generates Named Entity Count for each sentence that contains an edge
     """
 
-    def __init__(self, entity_type, prefix1):
+    def __init__(self, entity_type, prefix):
         self.entity_type = entity_type
         """type of entity"""
-        self.prefix1 = prefix1
+        self.prefix = prefix
 
 
     def generate(self, dataset, feature_set, is_training_mode):
         for edge in dataset.edges():
             entities = edge.part.get_entities_in_sentence(edge.sentence_id, self.entity_type)
             num_entities = len(entities)
-            feature_name = self.mk_feature_name(self.prefix1, self.entity_type, 'count', num_entities)
+            feature_name = self.mk_feature_name(self.prefix, self.entity_type, 'count', num_entities)
             self.add_to_feature_set(feature_set, is_training_mode, edge, feature_name, value=1)
 
 

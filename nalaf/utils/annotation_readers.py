a89b64f6d98c322a8b4b0e8bde3da8ff01bb7bb4
Juan Miguel Cejuela <i@juanmi.rocks>
2016-11-12 12:46:19 +0100

    fix

--- nalaf/utils/annotation_readers.py
+++ nalaf/utils/annotation_readers.py
@@ -103,11 +103,8 @@ class AnnJsonAnnotationReader(AnnotationReader):
 
                                 rel_id = relation['classId']
 
-                                # TODO
                                 e1 = part.get_entity(e1_start)
                                 e2 = part.get_entity(e2_start)
-                                e1 = None
-                                e2 = None
 
                                 rel = Relation(e1_start, e2_start, e1_text, e2_text, rel_id, e1, e2)
 

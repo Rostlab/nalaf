488d3e4ad094a623227f8e1368e350bc943d6c33
Juan Miguel Cejuela <i@juanmi.rocks>
2016-12-06 18:54:21 +0100

    add another utility

--- nalaf/features/relations/__init__.py
+++ nalaf/features/relations/__init__.py
@@ -67,6 +67,10 @@ class EdgeFeatureGenerator(FeatureGenerator):
         # print_debug(feature_name, field_prefix_feature, args)
         return feature_name
 
+    def gen_preffeatname_add(self, feature_set, is_training_mode, edge, field_prefix_feature, *args):
+        feature_name = self.gen_prefix_feat_name(field_prefix_feature, *args)
+        self.add_to_feature_set(feature_set, is_training_mode, edge, feature_name, value=1)
+
 
 from nalaf.features.relations import EdgeFeatureGenerator
 from nltk.stem import PorterStemmer

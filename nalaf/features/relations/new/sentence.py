70dd76d0dd699f2198d2563e0531777462ca312b
Juan Miguel Cejuela <i@juanmi.rocks>
2016-12-29 16:29:40 +0100

    first implementation

---
+++ nalaf/features/relations/new/sentence.py
@@ -0,0 +1,66 @@
+"""
+Sentence-based features implementation as succintly described in Shrikant's Master's Thesis (Section 4.5):
+https://github.com/juanmirocks/LocText-old-ShrikantThesis/files/474428/MasterThesis.pdf
+"""
+
+from nalaf.features.relations import EdgeFeatureGenerator
+
+
+class SentenceFeatureGenerator(EdgeFeatureGenerator):
+    """
+    Generate sentence-based features as defined in Master's Thesis page 40.
+
+    It strictly gets features that are based on the sentence: ONLY
+
+
+    BOW
+    lemmas
+    Num Entities of type X (prot, loc, or organism)
+    Numb of words/tokens
+    POS
+
+    """
+
+    def __init__(
+        self,
+        f_counts,
+        f_counts_in_between,
+        f_order,
+        f_bow,
+        f_pos,
+        f_tokens_count
+    ):
+
+        self.f_counts = f_counts
+        self.f_counts_in_between = f_counts_in_between
+        self.f_order = f_order
+        self.f_bow = f_bow
+        self.f_pos = f_pos
+        self.f_tokens_count = f_tokens_count
+
+
+    def generate(self, corpus, f_set, is_train):
+        for edge in corpus.edges():
+            sentence = edge.get_combined_sentence()
+
+            for e_class_id, entities in edge.get_any_entities_in_sentences(predicted=False).items():
+                count = str(len(entities))
+                # TODO feature select with integer value or binary representation ?
+                # self.add(f_set, is_train, edge, 'f_counts', 'binary', e_class_id, 'is', count)
+                self.add_with_value(f_set, is_train, edge, 'f_counts', count, 'int', e_class_id)
+
+            for e_class_id, entities in edge.get_any_entities_between_entities(predicted=False).items():
+                count = str(len(entities))
+                # TODO feature select with integer value or binary representation ?
+                # self.add(f_set, is_train, edge, 'f_counts_in_between', 'binary', e_class_id, 'is', count)
+                self.add_with_value(f_set, is_train, edge, 'f_counts_in_between', count, 'int', e_class_id)
+
+            order = edge.entity1.class_id < edge.entity2.class_id
+            if order:
+                self.add(f_set, is_train, edge, 'f_order')
+
+            for token in sentence:
+                self.add(f_set, is_train, edge, 'f_bow', token.features['lemma'])
+                self.add(f_set, is_train, edge, 'f_pos', token.features['pos'])
+
+            # self.add_with_value(f_set, is_train, edge, 'f_tokens_count', len(sentence))

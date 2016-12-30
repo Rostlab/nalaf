1ba30f0900194ee578e1888030f38dd77075f690
Juan Miguel Cejuela <i@juanmi.rocks>
2016-12-30 11:06:45 +0100

    add first implementation of revamped Floyd Warshall algorithm

---
+++ nalaf/utils/floyd_warshall.py
@@ -0,0 +1,61 @@
+"""
+Floyd-Warshall graph algorithm to compute the shortest paths between the dependency graphs of sentences.
+See: https://en.wikipedia.org/wiki/Floyd–Warshall_algorithm
+
+As of now, matrises are written fully. An obvious performance improvement is to write them sparsely.
+"""
+
+def compute_shortest_paths(sentence):
+    """
+    Compute the shortest paths of a sentence's dependency graph.
+
+    Returns tuple: (dist, then)
+        dist: matrix of minimal distances between pairs of tokens u, v
+        then: matrix of
+    Sugar code.
+    """
+    floyd_warshall_with_path_reconstruction(sentence_to_weight_matrix(sentence))
+
+
+def floyd_warshall_with_path_reconstruction(weight):
+    """
+    Compute the shortest distances and paths in a graph matrix representation.
+
+    Implementation of https://en.wikipedia.org/wiki/Floyd–Warshall_algorithm#Path_reconstruction
+
+    matrix 'then' is the equivalent of 'next'
+    """
+    V = len(weight)
+    dist = numpy.full([V, V], numpy.inf)
+    then = numpy.full([V, V], numpy.nan)
+
+    return dist, then
+
+
+def path(u, v, dist, then, sentence):
+    return []
+
+
+def sentence_to_weight_matrix(sentence):
+    """
+    Converts the dependency graph of a sentence of tokens into a weight matrix.
+    weight[u, v] = 0 iff u == v
+    weight[u, v] = 1 iff u != v and are_bidirectionaly_directly_connected(u, v) == True
+    weight[u, v] = 0 else
+    """
+
+    V = len(sentence)
+    weight = numpy.zeros([V, V], numpy.inf)
+
+    # for from_token in sentence:
+    #     for to_token, _ in from_token.features['dependency_to']:
+    #         u = from_token.features['id']
+    #         v = to_token.features['id']
+    #
+    #         weight[u, v] = 1
+    #         weight[v, u] = 1
+
+    # for u in V:
+    #     weight[u, u] = 0
+
+    return weight
---
+++ tests/utils/test_floyd_warshall.py
@@ -0,0 +1,79 @@
+import unittest
+from nalaf.structures.data import Dataset, Document, Part, Token, Label, Entity
+from nalaf.preprocessing.spliters import NLTKSplitter
+from nalaf import print_verbose, print_debug
+from nalaf.preprocessing.tokenizers import Tokenizer, NLTK_TOKENIZER, GenericTokenizer
+from nalaf.features import get_spacy_nlp_english
+from nalaf.preprocessing.parsers import Parser, SpacyParser
+import sys
+
+
+STUB_ENTITY_CLASS_ID_1 = 'e_x_1'
+STUB_ENTITY_CLASS_ID_2 = 'e_x_2'
+STUB_RELATION_CLASS_ID_2 = 'r_x'
+
+TEST_SENTENCES_SINGLE_ROOT = [
+    "Arabidopsis cotyledon - specific chloroplast biogenesis factor CYO1 is a protein disulfide isomerase .",
+    "FKBP12-rapamycin target TOR2 is a vacuolar protein with an associated phosphatidylinositol-4 kinase activity .",
+    "TMEM59 was found to be a ubiquitously expressed , Golgi - localized protein .",
+    "This indicates that Mdv1p possesses a Dnm1p - independent mitochondrial targeting signal .",
+    "Dnm1p - independent targeting of Mdv1p to mitochondria requires MDV2 .",
+    "The activated ROP11 recruits MIDD1 to induce local disassembly of cortical microtubules .",
+    "Conversely , cortical microtubules eliminate active ROP11 from the plasma membrane through MIDD1 .",
+    "GOLPH3L antagonizes GOLPH3 to determine Golgi morphology .",
+    "HERC2 coordinates ubiquitin - dependent assembly of DNA repair factors on damaged chromosomes .",
+    "Pivotal role of AtSUVH2 in heterochromatic histone methylation and gene silencing in Arabidopsis .",
+    "PHAX and CRM1 are required sequentially to transport U3 snoRNA to nucleoli .",
+    "CpSufE activates the cysteine desulfurase CpNifS for chloroplastic Fe - S cluster formation .",
+    "YMR313c/TGL3 encodes a novel triacylglycerol lipase located in lipid particles of Saccharomyces cerevisiae .",
+    "However , overexpression of ATG21 leads to CPY secretion .",
+    "PP2A colocalizes with shugoshin at centromeres and is required for centromeric protection .",
+]
+
+TEST_SENTENCES_MULTI_ROOT = [
+    # SS
+    "Import assays with pea ( Pisum sativum ) chloroplasts showed that PyrR and PyrD are taken up and proteolytically processed ."
+]
+
+class TestFloydWarshall(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.dataset = Dataset()
+
+        doc1 = Document()
+        cls.dataset.documents['TEST_SENTENCES_SINGLE_ROOT'] = doc1
+
+        for s in TEST_SENTENCES_SINGLE_ROOT:
+            part = Part(s)
+            doc1.parts[s] = part
+
+        doc2 = Document()
+        cls.dataset.documents['TEST_SENTENCES_MULTI_ROOT'] = doc2
+
+        for s in TEST_SENTENCES_MULTI_ROOT:
+            part = Part(s)
+            doc2.parts[s] = part
+
+        cls.nlp = get_spacy_nlp_english(load_parser=True)
+        cls.parser = SpacyParser(nlp)
+        cls.splitter = NLTKSplitter()
+        cls.tokenizer = GenericTokenizer(lambda string: (tok.text for tok in cls.nlp.tokenizer(string)))
+
+        cls.splitter.split(cls.dataset)
+        cls.tokenizer.tokenize(cls.dataset)
+        cls.parser.parse(cls.dataset)
+
+
+    def test_distance_u_u_is_0(self):
+        pass
+
+    def test_distance_u_v_is_v_u(self):
+        pass
+
+    def test_path_u_v_is_reverseof_v_u(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()

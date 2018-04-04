e70d8e4b622b268ac40049a5552fb91a02b8a0e3
Juan Miguel Cejuela <i@juanmi.rocks>
2018-04-04 13:40:32 +0200

    🥑 version bump: 0.5.0

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def license():
 
 setup(
     name='nalaf',
-    version='0.4.0',
+    version='0.5.0',
     description='Natural Language Framework, for NER and RE',
     long_description=readme(),
     classifiers=[

0922dcce66b064804a90f2a138003d3a2651095b
Juan Miguel Cejuela <i@juanmi.rocks>
2018-07-23 14:05:12 +0200

    bump 0.5.4 🏖

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def license():
 
 setup(
     name='nalaf',
-    version='0.5.2',
+    version='0.5.4',
     description='Natural Language Framework, for NER and RE',
     long_description=readme(),
     classifiers=[

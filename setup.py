46ec497bedfe4b34f08b828af2138cf223e5b529
Juan Miguel Cejuela <i@juanmi.rocks>
2019-05-23 20:21:14 +0200

    fix deps

--- setup.py
+++ setup.py
@@ -30,20 +30,20 @@ setup(
     packages=find_packages(exclude=['tests']),
     install_requires=[
         # ML
-        'numpy >= 1.11.2, <= 1.11.3',  # Tried with 1.13.3, but doesn't work with spacy 1.2.0
-        'scipy >= 0.18.1, <= 0.19.1',  # In newer versions they remove scipy.maxentropy
-        'scikit-learn >= 0.18.1, <= 0.18.2',
-        'spacy == 1.2.0',
-        'nltk >= 3.2.1',
+        'numpy == 1.16.*',
+        'scipy >= 0.18.1, < 1.3',
         'gensim >= 0.13.3, <= 0.13.4.1',  # In 1.0.0 they move .vocab: https://github.com/RaRe-Technologies/gensim/blob/master/CHANGELOG.md#100-2017-02-24
+        'scikit-learn >= 0.18.1, <= 0.20.3',
+        'spacy == 1.2.0',
         'python-crfsuite >= 0.9.3, <= 0.9.6',
+        'nltk >= 3.2.1',
 
         # Other
         'beautifulsoup4 >= 4.5.1',
         'requests >= 2.21.0',
         'progress >= 1.2',
         'hdfs == 2.1.0',
-        'urllib3 <1.25, >=1.20'  # force, due to dependency problems with botocore
+        'urllib3 >=1.20, <1.25'  # force, due to dependency problems with botocore
     ],
     include_package_data=True,
     zip_safe=False,

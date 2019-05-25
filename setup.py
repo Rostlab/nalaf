de597fae8078104341ad793c16740c3516b7d2f1
Juan Miguel Cejuela <i@juanmi.rocks>
2019-05-25 15:07:49 +0200

    bump 0.5.8 to force build

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def license():
 
 setup(
     name='nalaf',
-    version='0.5.7',
+    version='0.5.8',
     description='Natural Language Framework, for NER and RE',
     long_description=readme(),
     long_description_content_type="text/markdown",

31cabd0b3eff7617d0253d204504db0d5c4b11a6
Juan Miguel Cejuela <juanmi@tagtog.net>
2020-11-27 14:52:42 +0100

    Declare this to be for now a SNAPSHOT version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def readme():
 
 setup(
     name='nalaf',
-    version='0.5.11',
+    version='0.5.11-SNAPSHOT',
     description='Natural Language Framework, for NER and RE',
     long_description=readme(),
     long_description_content_type="text/markdown",
